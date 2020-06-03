34adcd9657b9837cec5ac2051284faee9cae52f5
hewel <hewelzei@qq.com>
2020-06-03 13:55:19 +0800

    perf: 更准确识别头像框元素

--- src/initial/compatibleStyles.js
+++ src/initial/compatibleStyles.js
@@ -1,7 +1,7 @@
 import { css } from 'emotion/dist/emotion.umd.min'
 
 function compatibleStyles() {
-  const avatarDecorate = document.querySelector('.avatar-decorate')
+  const avatarDecorate = document.querySelector('.vp-container .avatar-decorate')
   avatarDecorate.classList.add(
     css`
       max-width: inherit;
